c05a28db1e456004b38f9a795be5b52030856ae6
Mariano <marianonovak@hotmail.com>
2025-06-06 12:22:11 -0300

    carga de imagenes productos

--- service/productService.js
+++ service/productService.js
@@ -2,12 +2,12 @@ const Product = require('../model/Product');
 
 // Productos hardcodeados por ahora. Proximos a implementar via bbdd
 const products = [
-  new Product(1, 'Notebook Lenovo', 800, 'notebook', '/img/products/lenovo.jpg'),
-  new Product(2, 'Macbook Air', 1200, 'notebook', '/img/products/macbook.jpg'),
-  new Product(3, 'Notebook HP', 700, 'notebook', '/img/products/hp.png'),
-  new Product(4, 'PC Gamer AMD', 1500, 'pc', '/img/products/pc_amd.png'),
-  new Product(5, 'PC Intel i7', 1400, 'pc', '/img/products/pc_i7.png'),
-  new Product(6, 'PC Básica', 600, 'pc', '/img/products/pc_basic.png'),
+  new Product(1, 'Notebook Lenovo', 800000, 'notebook', '/img/products/lenovo.jpg'),
+  new Product(2, 'Macbook Air', 1200000, 'notebook', '/img/products/macbook.jpg'),
+  new Product(3, 'Notebook HP', 700000, 'notebook', '/img/products/hp.png'),
+  new Product(4, 'PC Gamer AMD', 600000, 'pc', '/img/products/pc_amd.jpg'),
+  new Product(5, 'PC Gamer Intel', 800000, 'pc', '/img/products/pc_intel.jpg'),
+  new Product(6, 'PC Oficina', 350000, 'pc', '/img/products/pc_oficina.jpg'),
 ];
 
 module.exports = {
