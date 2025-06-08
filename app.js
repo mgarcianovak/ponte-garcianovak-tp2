726d721f63bd73f731f64f18a60196588cbf3e42
Lucas Nicolás Ponte <lucasponteponte204@gmail.com>
2025-06-08 19:26:34 -0300

    cambio el app.js

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Middleware para JSON
 app.use(express.json());
 
 // Archivos estáticos (HTML, CSS, JS)
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, '/static')));
 
 // Rutas de la API
 const productRoutes = require('./routes/api/products');
 app.use('/api', productRoutes);
+
+// Ruta raíz index
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static/html/index.html'));
+});
+
+// Ruta raíz productos
+app.get('/products.html', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/html/products.html'));
+});
+
+// Ruta raíz carrito
+app.get('/carrito.html', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/html/carrito.html'));
+});
 
 // Catch-all para rutas no encontradas
 app.use((req, res) => {
