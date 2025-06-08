6fa86a2f2a6863ca154e8ce3919d8b4e78e59ea1
Mariano <marianonovak@hotmail.com>
2025-06-08 13:25:15 -0300

    pruebas

--- client/service/productService.js
+++ client/service/productService.js
@@ -1,16 +1,4 @@
-const Product = require('../model/Product');
-
-// Productos hardcodeados por ahora. Proximos a implementar via bbdd
-const products = [
-  new Product(1, 'Notebook Lenovo', 800000, 'notebook', '/img/products/lenovo.jpg'),
-  new Product(2, 'Macbook Air', 1200000, 'notebook', '/img/products/macbook.jpg'),
-  new Product(3, 'Notebook HP', 700000, 'notebook', '/img/products/hp.png'),
-  new Product(4, 'PC Gamer AMD', 600000, 'pc', '/img/products/pc_amd.jpg'),
-  new Product(5, 'PC Gamer Intel', 800000, 'pc', '/img/products/pc_intel.jpg'),
-  new Product(6, 'PC Oficina', 350000, 'pc', '/img/products/pc_oficina.jpg'),
-];
-
-const obtenerProductos = async () => {
+const getAll = async () => {
   try {
     const url = `http://localhost:3001/product`;
     const response = await fetch(url);
@@ -22,12 +10,35 @@ const obtenerProductos = async () => {
   }
 }
 
+const getByCategory = async (category) => {
+  try {
+    const url = `http://localhost:3001/product/category/${category}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+const getById = async (id) => {
+  try {
+    const url = `http://localhost:3001/product/id/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
 module.exports = {
-  getAllProducts: async () => await obtenerProductos(),
+  getAllProducts: async () => await getAll(),
 
-  getProductsByCategory: (category) =>
-    products.filter(product => product.category === category),
+  getProductsByCategory: async (category) => await getByCategory(category),
 
-  getProductById: (id) =>
-    products.find(product => product.id === parseInt(id)),
+  getProductById: async (id) => await getById(id)
 };
