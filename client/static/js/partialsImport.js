b25ef08cb7484e07225998493a55661412854525
Mariano <marianonovak@hotmail.com>
2025-06-22 15:06:31 -0300

    borrar carrito y nombre de localStorage al cerrar sesion

--- client/static/js/partialsImport.js
+++ client/static/js/partialsImport.js
@@ -1,14 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navbarTarget = document.getElementById("navbar-partial");
-  const footerTarget = document.getElementById("footer-partial");
-
   if (navbarTarget) {
     fetch("/html/partials/navbar.html")
       .then(res => res.text())
-      .then(html => navbarTarget.innerHTML = html)
+      .then(html => {
+        navbarTarget.innerHTML = html;
+        const logoutBtn = document.getElementById("logout-btn");
+        if (logoutBtn) {
+          logoutBtn.addEventListener("click", () => {
+            localStorage.removeItem("cart");
+            localStorage.removeItem("clientName");
+          });
+        }
+      })
       .catch(err => console.error("Error cargando navbar:", err));
   }
 
+  const footerTarget = document.getElementById("footer-partial");
   if (footerTarget) {
     fetch("/html/partials/footer.html")
       .then(res => res.text())
