7965adb99d338f942478b906e7db6398bfafe2f2
Lucas Nicolás Ponte <lucasponteponte204@gmail.com>
2025-06-23 20:39:15 -0300

    soluciono detalle de imagen

--- server/service/productService.js
+++ server/service/productService.js
@@ -24,12 +24,19 @@ module.exports = {
 };
 
 /*
+la base de datos tiene estos productos 
 const products = [
-  new Product(1, 'Notebook Lenovo', 800000, 'notebook', '/img/products/lenovo.jpg'),
-  new Product(2, 'Macbook Air', 1200000, 'notebook', '/img/products/macbook.jpg'),
-  new Product(3, 'Notebook HP', 700000, 'notebook', '/img/products/hp.png'),
-  new Product(4, 'PC Gamer AMD', 600000, 'pc', '/img/products/pc_amd.jpg'),
-  new Product(5, 'PC Gamer Intel', 800000, 'pc', '/img/products/pc_intel.jpg'),
-  new Product(6, 'PC Oficina', 350000, 'pc', '/img/products/pc_oficina.jpg'),
+  new Product(1, 'Lenovo Thinkpad', 800000, 'notebook', '/img/products/notebooks/lenovo.jpg', true),
+  new Product(2, 'Macbook Air', 1200000, 'notebook', '/img/products/notebooks/macbook.jpg', true),
+  new Product(3, 'HP 255', 700000, 'notebook', '/img/products/notebooks/hp.png', true),
+  new Product(4, 'ASUS X515EA', 1100000, 'notebook', '/img/products/notebooks/asus.jpg', true),
+  new Product(5, 'Acer Inspire 3', 650000, 'notebook', '/img/products/notebooks/acer.jpg', true),
+  new Product(6, 'Sony VAIO 14.1"', 750000, 'notebook', '/img/products/notebooks/vaio.jPg', true),
+  new Product(7, 'PC Oficina', 350000, 'pc', '/img/products/pc/oficina.jpg', true),
+  new Product(8, 'PC AMD Ryzen 5', 450000, 'pc', '/img/products/pc/r5.jpg', true),
+  new Product(9, 'PC Intel Core i5', 500000, 'pc', '/img/products/pc/i5.jpg', true),
+  new Product(10, 'Mini Pc Intel Nuc 10 Core I7', 1600000, 'pc', '/img/products/pc/mini.jpg', true),
+  new Product(11, 'PC AMD Ryzen 7', 600000, 'pc', '/img/products/pc/r7.jpg', true),
+  new Product(12, 'PC Intel Core i7', 700000, 'pc', '/img/products/pc/i7.jpg', true)
 ];
 */
