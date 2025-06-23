ab79222cfc31b72317a514193631055bebce3072
Lucas Nicolás Ponte <lucasponteponte204@gmail.com>
2025-06-23 16:09:49 -0300

    guarda el usuario en bdd

--- server/controller/usuarioController.js
+++ server/controller/usuarioController.js
@@ -1,15 +1,13 @@
 //crear un usuario en la base de datos 
-const { Usuarios } = require('../models/usuarios');
+const  Usuarios  = require('../models/usuarios');
 
-//crear un usuario en la base de datos
 exports.crearUsuario = async (req, res) => {
   try {
     const { nombre } = req.body;
     const nuevoUsuario = await Usuarios.create({ nombre });
     res.status(201).json(nuevoUsuario);
   } catch (error) {
-    console.error("Error al registrar usuario:", error); // 👈 Mostralo en consola
+    console.error("Error al registrar usuario:", error);
     res.status(500).json({ error: "Error al registrar usuario" });
   }
 };
-//revisar
