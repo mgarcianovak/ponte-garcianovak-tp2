66bbd33dec1441fa0a1225cb2eac7f87c527cbd3
mariangnovak <marianonovak@hotmail.com>
2025-06-30 18:21:22 -0300

    final minor fixes

--- server/service/usuariosService.js
+++
@@ -1,6 +0,0 @@
-const Usuarios = require('../models/usuarios');
-// Este servicio se encarga de gestionar los usuarios
-// y contiene la lógica para crear un nuevo usuario en la base de datos.
-exports.crearUsuario = async (nombre) => {
-  return await Usuarios.create({ nombre });
-};
